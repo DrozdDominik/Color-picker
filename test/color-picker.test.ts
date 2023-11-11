51ab5ffd4647f9421337f2862f06a19f924e6336
Dominik Drozd <dominik.drozd@thefarm51.com>
2023-11-11 13:05:46 +0100

    Added gradient square tests

--- test/color-picker.test.ts
+++ test/color-picker.test.ts
@@ -1,27 +1,41 @@
 import { html } from 'lit';
-import { fixture, expect } from '@open-wc/testing';
+import { fixture, expect, aTimeout } from '@open-wc/testing';
 import { ColorPicker } from '../src/ColorPicker.js';
 import '../src/color-picker.js';
+import { Colord, colord } from 'colord';
 
 describe('ColorPicker', () => {
-  it('has a default header "Hey there" and counter 5', async () => {
+  const hue = 1
+  const saturation = 100
+  const value = 100
+  const selectedColorValue = colord({ h: hue, s: saturation, v: value }).toRgbString()
+
+  it('should has properties initialized correctly', async () => {
     const el = await fixture<ColorPicker>(html`<color-picker></color-picker>`);
 
-    expect(el.header).to.equal('Hey there');
-    expect(el.counter).to.equal(5);
+    expect(el.hue).to.equal(hue);
+    expect(el.saturation).to.equal(saturation);
+    expect(el.value).to.equal(value);
+    expect(el.selectedColor).to.instanceOf(Colord);
+    expect(el.selectedColor.toRgbString()).to.equal(selectedColorValue);
   });
 
-  it('increases the counter on button click', async () => {
+  it('should change selectedColor value when gradient-square clicked', async () => {
     const el = await fixture<ColorPicker>(html`<color-picker></color-picker>`);
-    el.shadowRoot!.querySelector('button')!.click();
+    
+    expect(el.selectedColor.toRgbString()).to.equal(selectedColorValue);
 
-    expect(el.counter).to.equal(6);
-  });
+    const clickEvent = new MouseEvent('click', {
+      clientX: 100,
+      clientY: 200
+    });
 
-  it('can override the header via attribute', async () => {
-    const el = await fixture<ColorPicker>(html`<color-picker header="attribute header"></color-picker>`);
+    (el.shadowRoot!.querySelector('.gradient-square') as HTMLElement).dispatchEvent(clickEvent);
 
-    expect(el.header).to.equal('attribute header');
+    await aTimeout(50);
+
+    expect(el.selectedColor).to.instanceOf(Colord);
+    expect(el.selectedColor.toRgbString()).not.to.equal(selectedColorValue);
   });
 
   it('passes the a11y audit', async () => {
