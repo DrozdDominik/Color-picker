27b54b475946637e85cedb9fed26f32ca4931405
Dominik Drozd <dominik.drozd@thefarm51.com>
2023-11-12 16:58:27 +0100

    Added ability to control the size of the component using an attribute

--- src/ColorPicker.ts
+++ src/ColorPicker.ts
@@ -1,40 +1,45 @@
 import { html, css, LitElement } from 'lit';
 import { property, query } from 'lit/decorators.js';
-import { colord } from "colord";
+import { colord } from 'colord';
 
 export class ColorPicker extends LitElement {
   static styles = css`
+    :host {
+      --size: var(--picker-width, 750px)
+    }
     .container {
+      width: var(--size);
+      height: calc(var(--size) / 3);
       display: flex;
       flex-direction: column;
       align-items: center;
       justify-content: space-between;
     }
     .main {
-      margin-bottom:20px;
+      margin-bottom: calc(var(--size) / 30);
       display: flex;
       justify-content: space-between;
     }
     .gradient-square {
-      margin: 0 10px;
+      margin: 0 calc(var(--size) / 75);
       position: relative;
-      width: 200px;
-      height: 200px;
+      width: calc(var(--size) / 3.75);
+      height: calc(var(--size) / 3.75);
       cursor: crosshair;
     }
     .selector {
       position: absolute;
       border: 1px solid #fff;
       border-radius: 50%;
-      width: 10px;
-      height: 10px;
+      width: calc(var(--size) / 75);
+      height: calc(var(--size) / 75);
       transform: translate(-50%, -50%);
       pointer-events: none;
     }
     .slider {
       display: block;
-      width: 60%;
-      height: 30px;
+      width: 80%;
+      height: calc(var(--size) / 30);
       background: linear-gradient(
         to right,
         red,
@@ -48,35 +53,44 @@ export class ColorPicker extends LitElement {
     }
     input[type='range'] {
       width: 100%;
-      height: 30px;
+      height: calc(var(--size) / 25);
       background: transparent;
       -webkit-appearance: none;
     }
     input[type='range']::-webkit-slider-thumb {
       -webkit-appearance: none;
       appearance: none;
-      width: 15px;
-      height: 30px;
+      width: calc(var(--size) / 50);
+      height: calc(var(--size) / 25);
       background: white;
       cursor: pointer;
     }
     .color-preview {
-      width: 100px;
-      height: 100px;
-      margin: 50px 10px;
+      width: calc(var(--size) / 7.5);
+      height: calc(var(--size) / 7.5);
+      margin: calc(var(--size) / 15) calc(var(--size) / 75);
     }
     .inputs {
-      margin: 0 10px;
+      width: calc(var(--size) / 2.25);
+      height: calc(var(--size) / 3.75);
+      margin: 0 calc(var(--size) / 75);
       display: flex;
       flex-direction: column;
       justify-content: center;
     }
 
     .rgb-inputs {
-      margin-bottom: 10px
+      margin-bottom: calc(var(--size) / 75);
+    }
+
+    .inputs {
+      font-size: calc(var(--size) / 50);
     }
 
     .inputs input {
+      width: calc(var(--size) / 11);
+      height: calc(var(--size) / 37.5);
+      font-size: calc(var(--size) / 60);
       display: inline-block;
       border: 1px solid #ccc;
       border-radius: 4px;
@@ -84,7 +98,7 @@ export class ColorPicker extends LitElement {
       text-align: center;
     }
 
-    .inputs input:focus { 
+    .inputs input:focus {
       outline: none;
     }
 
@@ -99,10 +113,6 @@ export class ColorPicker extends LitElement {
     .blue:focus {
       background-color: #aec6cf;
     }
-
-    .hex-input {
-      width: 80px
-    }
   `;
 
   @property({ type: Number })
@@ -115,56 +125,87 @@ export class ColorPicker extends LitElement {
   value = 100;
 
   @property({ attribute: false })
-  selectedColor = colord({ h: this.hue, s: this.saturation, v: this.value })
+  selectedColor = colord({ h: this.hue, s: this.saturation, v: this.value });
 
   @query('.red')
-  _red?: HTMLInputElement
+  _red?: HTMLInputElement;
 
   @query('.green')
-  _green?: HTMLInputElement
+  _green?: HTMLInputElement;
 
   @query('.blue')
-  _blue?: HTMLInputElement
+  _blue?: HTMLInputElement;
 
   render() {
     return html`
       <div class="container">
         <div class="main">
-          <div class="gradient-square"
+          <div
+            class="gradient-square"
             style="background: 
               linear-gradient(to top, black, rgba(0, 0, 0, 0)),
-              linear-gradient(to right, white, ${colord({ h: this.hue, s: 100, v: 100 }).toRgbString()})"
-            @click="${this.handleClick}">
-              <div
-                  class="selector"
-                  style="left: ${this.saturation}%; top: ${100 - this.value}%"
-                >
-              </div>
+              linear-gradient(to right, white, ${colord({
+              h: this.hue,
+              s: 100,
+              v: 100,
+            }).toRgbString()})"
+            @click="${this.handleClick}"
+          >
+            <div
+              class="selector"
+              style="left: ${this.saturation}%; top: ${100 - this.value}%"
+            ></div>
           </div>
           <div
-              class="color-preview"
-              style="background-color: ${this.selectedColor.toRgbString()};"
-            >
-          </div>
+            class="color-preview"
+            style="background-color: ${this.selectedColor.toRgbString()};"
+          ></div>
           <div class="inputs">
             <div class="rgb-inputs">
               <p><b>RGB values</b></p>
               <label>
                 Red
-                <input class="red" type="number" .value="${this.selectedColor.toRgb().r.toString()}" min="0" max="255" @input="${this.handleInput}">
+                <input
+                  class="red"
+                  type="number"
+                  .value="${this.selectedColor.toRgb().r.toString()}"
+                  min="0"
+                  max="255"
+                  @input="${this.handleInput}"
+                />
               </label>
               <label>
                 Green
-                <input class="green" type="number" .value="${this.selectedColor.toRgb().g.toString()}" min="0" max="255" @input="${this.handleInput}">
+                <input
+                  class="green"
+                  type="number"
+                  .value="${this.selectedColor.toRgb().g.toString()}"
+                  min="0"
+                  max="255"
+                  @input="${this.handleInput}"
+                />
               </label>
               <label>
                 Blue
-                <input class="blue" type="number" .value="${this.selectedColor.toRgb().b.toString()}" min="0" max="255" @input="${this.handleInput}">
+                <input
+                  class="blue"
+                  type="number"
+                  .value="${this.selectedColor.toRgb().b.toString()}"
+                  min="0"
+                  max="255"
+                  @input="${this.handleInput}"
+                />
               </label>
             </div>
             <div>
-              <label><b>Hex value</b>
-                <input class="hex-input" type="text" .value="${this.selectedColor.toHex()}" readonly>
+              <label
+                ><b>Hex value</b>
+                <input
+                  class="hex-input"
+                  type="text"
+                  .value="${this.selectedColor.toHex()}"
+                  readonly
+                />
               </label>
             </div>
           </div>
@@ -188,16 +229,28 @@ export class ColorPicker extends LitElement {
     const rect = (event.currentTarget as HTMLElement).getBoundingClientRect();
     this.saturation = ((event.clientX - rect.left) / rect.width) * 100;
     this.value = 100 - ((event.clientY - rect.top) / rect.height) * 100;
-    this.selectedColor = colord({ h: this.hue, s: this.saturation, v: this.value });
+    this.selectedColor = colord({
+      h: this.hue,
+      s: this.saturation,
+      v: this.value,
+    });
   }
 
   handleHueChange(event: Event) {
     this.hue = parseInt((event.target as HTMLInputElement).value);
-    this.selectedColor = colord({ h: this.hue, s: this.saturation, v: this.value });
+    this.selectedColor = colord({
+      h: this.hue,
+      s: this.saturation,
+      v: this.value,
+    });
   }
 
   handleInput() {
-    const newColor = colord({ r: Number(this._red?.value), g: Number(this._green?.value), b: Number(this._blue?.value) });
+    const newColor = colord({
+      r: Number(this._red?.value),
+      g: Number(this._green?.value),
+      b: Number(this._blue?.value),
+    });
     this.selectedColor = newColor;
     const newColorHsv = newColor.toHsv();
     this.hue = newColorHsv.h;
